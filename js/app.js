0bb3d5bf3f7201b346435ba848666c03389d2e7e
Md Wahidul Alam <103446433+Muhammad-Wahidul-Alam@users.noreply.github.com>
2022-08-12 07:37:43 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('.banner_slides').slick({
+
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        // autoplay: true,
+        // autoplaySpeed: 1000,
+        arrows: true,
+        prevArrow: `.prevArrow`,
+        nextArrow: `.nextArrow`,
+        dots: true,
+
+
+        
+    });
+
+
+
+})
